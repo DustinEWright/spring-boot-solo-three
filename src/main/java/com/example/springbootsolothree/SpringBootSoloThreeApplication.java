326958d5fcd4fa664f98680e73b966d513ed8f01
Dustin Wright <nzeroxpd@gmail.com>
2020-07-01 14:09:28 -0500

    Added starting files.

---
+++ src/main/java/com/example/springbootsolothree/SpringBootSoloThreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsolothree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoloThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoloThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootsolothree/SpringBootSoloThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsolothree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoloThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
