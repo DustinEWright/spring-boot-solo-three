0d570ea033e12857160c22e50016385122322f83
Dustin Wright <nzeroxpd@gmail.com>
2020-07-01 15:19:58 -0500

    Added @Component annotation to BootStrapData class.  Works as expected.

--- src/main/java/com/example/springbootsolothree/bootstrap/BootStrapData.java
+++ src/main/java/com/example/springbootsolothree/bootstrap/BootStrapData.java
@@ -1,7 +1,8 @@
 package com.example.springbootsolothree.bootstrap;
-
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
