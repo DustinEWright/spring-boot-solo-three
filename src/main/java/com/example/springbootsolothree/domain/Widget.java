71eb9b85f1559386a4f5e3012078a777414af8d6
Dustin Wright <nzeroxpd@gmail.com>
2020-07-01 14:29:26 -0500

    Filled in Widget and WidgetRepository.

--- src/main/java/com/example/springbootsolothree/domain/Widget.java
+++ src/main/java/com/example/springbootsolothree/domain/Widget.java
@@ -1,4 +1,76 @@
 package com.example.springbootsolothree.domain;
 
+import jdk.jfr.Enabled;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by DW 07/01/20
+ */
+@Enabled
 public class Widget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String color;
+
+    public Widget() {
+    }
+
+    public Widget(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Widget{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Widget widget = (Widget) o;
+
+        return id != null ? id.equals(widget.id) : widget.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
--- src/main/java/com/example/springbootsolothree/repositories/WidgetRepository.java
+++ src/main/java/com/example/springbootsolothree/repositories/WidgetRepository.java
@@ -1,4 +1,9 @@
 package com.example.springbootsolothree.repositories;
+import com.example.springbootsolothree.domain.Widget;
+import org.springframework.data.repository.CrudRepository;
 
-public interface WidgetRepository {
+/**
+ * Created by DW 07/01/20
+ */
+public interface WidgetRepository extends CrudRepository<Widget, Long> {
 }
