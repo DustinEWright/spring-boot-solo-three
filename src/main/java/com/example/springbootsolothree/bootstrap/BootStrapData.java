9e4a8e26c1e0248390014fbde7c4818932ff12dc
Dustin Wright <nzeroxpd@gmail.com>
2020-07-03 16:00:49 -0500

    Added Widget entities to the widgetRepository and output the ToString() on each.  Woks as expected.

--- src/main/java/com/example/springbootsolothree/bootstrap/BootStrapData.java
+++ src/main/java/com/example/springbootsolothree/bootstrap/BootStrapData.java
@@ -1,11 +1,39 @@
 package com.example.springbootsolothree.bootstrap;
+import com.example.springbootsolothree.domain.Widget;
+import com.example.springbootsolothree.repositories.WidgetRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
+
+    private final WidgetRepository widgetRepository;
+
+    public BootStrapData(WidgetRepository widgetRepository) {
+        this.widgetRepository = widgetRepository;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Hello World.");
+
+        Widget fred = new Widget("Fred", "orange");
+        Widget dave = new Widget();
+        dave.setName("Dave");
+        dave.setColor("White");
+
+        widgetRepository.save(fred);
+        widgetRepository.save(dave);
+
+        System.out.println("\nBootstrap starting...\n manual output");
+        System.out.println("Number of widgets: " + widgetRepository.count());
+        System.out.println("Widget 01: " + fred.toString());
+        System.out.println("Widget 02: " + dave.toString());
+        System.out.println("\nFor loop attempt");
+
+        for (int i = 0; i < widgetRepository.count(); i++) {
+            System.out.printf("Test : " + widgetRepository.findById(Long.valueOf(i)).toString());
+        }
+
+        System.out.println("\n\nBootstrap ending....");
     }
 }
