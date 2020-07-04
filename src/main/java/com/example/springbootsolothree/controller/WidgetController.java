eca97f8bd201b4ac3c8f1848bfcac1ccb75e831a
Dustin Wright <nzeroxpd@gmail.com>
2020-07-04 15:14:52 -0500

    Added @Controller annotation and getWidget() to the WidgetController class.

--- src/main/java/com/example/springbootsolothree/controller/WidgetController.java
+++ src/main/java/com/example/springbootsolothree/controller/WidgetController.java
@@ -1,8 +1,17 @@
 package com.example.springbootsolothree.controller;
 
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by DW 07/04/20
  */
+@Controller
 public class WidgetController {
+
+    public String getWidget() {
+
+        return "widget";
+    }
+
 }
