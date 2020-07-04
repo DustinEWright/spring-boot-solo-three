aab55a5621c12c8332629fe6f853119580434544
Dustin Wright <nzeroxpd@gmail.com>
2020-07-04 17:43:54 -0500

    Added POST Rquest mapping to WidgetController and a form to list.html.  Works as expected.

--- src/main/java/com/example/springbootsolothree/controller/WidgetController.java
+++ src/main/java/com/example/springbootsolothree/controller/WidgetController.java
@@ -1,14 +1,19 @@
 package com.example.springbootsolothree.controller;
 
 
+import com.example.springbootsolothree.domain.Widget;
 import com.example.springbootsolothree.repositories.WidgetRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 3) Have a HTML form that does a HTTP POST to a controller that inserts a row into the database
  * (confirm with h2-console)
+ * https://www.baeldung.com/spring-requestmapping
  * 4) Have a HTTP GET controller endpoint that outputs to the web browser each row in a table
  */
 
@@ -24,10 +29,20 @@ public class WidgetController {
         this.widgetRepository = widgetRepository;
     }
 
+    // 	public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+    @PostMapping("/widget")
+    public String postWidget(Model model, Widget widget) {
+        widgetRepository.save(widget);
+        Iterable<Widget> list = widgetRepository.findAll();
+        model.addAttribute("widgets", list);
+
+        return "widget/list";
+    }
+
     @RequestMapping("/widget")
     public String getWidget(Model model) {
-
-        model.addAttribute("widget", widgetRepository.findAll());
+        Iterable<Widget> list = widgetRepository.findAll();
+        model.addAttribute("widgets", list);
 
         return "widget/list";
     }
